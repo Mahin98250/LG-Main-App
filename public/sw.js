c488de264f1066301cc3297216f69155bf151058
Mahin98250 <patelmahin140@gmail.com>
2026-08-10 22:29:46 +0530

    feat: add offline PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE = "learners-guide-v1";
+const APP_SHELL = ["./", "./manifest.webmanifest", "./pwa-icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const request = event.request;
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(request).then((cached) => cached || caches.match("./"))),
+  );
+});
