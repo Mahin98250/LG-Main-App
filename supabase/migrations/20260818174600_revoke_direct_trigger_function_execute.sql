794abc1208c6b1c878e0aabb1bd0b5206146cdf3
Mahin98250 <patelmahin140@gmail.com>
2026-08-18 17:56:38 +0530

    chore: correct migration filename timestamp

--- supabase/migrations/20260818174600_revoke_direct_trigger_function_execute.sql
+++
@@ -1,8 +0,0 @@
--- Trigger functions do not need client EXECUTE privileges.
--- Revoking these grants prevents direct RPC invocation of internal trigger helpers.
-
-revoke execute on function public.set_material_folder_updated_at() from public, anon, authenticated;
-revoke execute on function public.sync_admin_compat_columns() from public, anon, authenticated;
-
-grant execute on function public.set_material_folder_updated_at() to postgres;
-grant execute on function public.sync_admin_compat_columns() to postgres;
